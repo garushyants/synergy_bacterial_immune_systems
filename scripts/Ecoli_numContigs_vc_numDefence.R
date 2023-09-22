e8cd2c20b46a76d2d7658ccabe514759ed9e2da6
Sofya Garushyants <garushyants@github.com>
2023-09-22 12:32:38 -0400

    Ecoli contigs vs num defence recalculated

--- scripts/Ecoli_numContigs_vc_numDefence.R
+++ scripts/Ecoli_numContigs_vc_numDefence.R
@@ -5,56 +5,53 @@ library(ggplot2)
 
 path<-getwd()
 setwd(path)
-setwd("../../20230920_Ecoli_number_of_contigs_vs_defence_systems/")
+
 
 ###read tree
-tree<-read.tree("Ecoli_tree_rapidnj.rM2.treeshrink_corrected.nwk")
+tree<-read.tree("../data/Ecoli_tree_rapidnj.rM2.treeshrink_corrected.nwk")
 GenomesToKeep<-tree$tip.label
 rep_str = c("'"='','.fna'='')
 GenomesToKeep<-str_replace_all(GenomesToKeep,rep_str)
 
-###read BV-BRC data
-EcoliBVBRC<-read.csv("Ecoli_BVBRC_genome_20230920.csv", header=T)
-AssemblySummary<-read.csv("assembly_summary_refseq_ecoli.csv", header=T)
-AssemblySummary$IDtoMatch<-ifelse(AssemblySummary$paired_asm_comp !="", AssemblySummary$paired_asm_comp,
-                                  AssemblySummary$assembly_accession)
-DataToProcess<-merge(AssemblySummary[,c("assembly_accession","IDtoMatch")],
-                     EcoliBVBRC[,c("Assembly.Accession","Contigs","Size","Plasmids")],
-                     by.y="Assembly.Accession", by.x="IDtoMatch")
-DataToProcessFinal<-subset(DataToProcess,
-                           DataToProcess$assembly_accession %in% GenomesToKeep)
+###read contig count data
+EcoliContigs<-read.csv("../data/Ecoli_numContigs_vs_numDefence/Ecoli_contigs.txt", sep="\t", header=F)
+EcoliContigs$genome<-str_replace(EcoliContigs$V1,".fna","")
+EcoliContigsFilt<-subset(EcoliContigs,
+                         EcoliContigs$genome %in% GenomesToKeep)
+
+AssemblySummary<-read.csv("../data/Ecoli_numContigs_vs_numDefence/assembly_summary_refseq_ecoli.csv", header=T)
 
 ######Read defence data
-EcoliDefense<-read.csv("complete_prophage_platon_26k_all7.csv")
+EcoliDefense<-read.csv("../data/26k_Ecoli_with_prophages.csv")
 
 EcoliDefenseWithContigsCounts<-subset(EcoliDefense,
-                                      EcoliDefense$genome %in% DataToProcess$assembly_accession)
+                                      EcoliDefense$genome %in% EcoliContigsFilt$genome &
+                                        EcoliDefense$genome %in% AssemblySummary$assembly_accession)
 
 EcoliDefCounts<-EcoliDefenseWithContigsCounts %>% group_by(genome) %>%
   summarize(NumDefPred=n())
 
 #####
 #Plot
-ForPlot<-merge(EcoliDefCounts,DataToProcess, by.x = "genome", by.y = "assembly_accession")
-ForPlot$Plasmids[is.na(ForPlot$Plasmids)]<-0
-ForPlot$allcontigs<-ForPlot$Contigs+ForPlot$Plasmids
+ForPlot<-merge(EcoliDefCounts,EcoliContigs[,c("genome","V2")], by = "genome")
+
 
 ###
-CorCoef<-cor.test(ForPlot$Contigs, ForPlot$NumDefPred,
+CorCoef<-cor.test(ForPlot$V2, ForPlot$NumDefPred,
                   method="spearman",
                   alternative = "two.sided",
-                  conf.level=0.99,
+                  conf.level=0.95,
                   exact = F)
 pvalue<-ifelse(CorCoef$p.value == 0, "p-value < 2.2e-16",
                paste("p-value =",format(CorCoef$p.value, 2, digits=3)))
 
 
 Plot<-ggplot(data=ForPlot,
-       aes(x=Contigs,
-           y=NumDefPred))+
-  geom_point(alpha=.2,
+             aes(x=V2,
+                 y=NumDefPred))+
+  geom_point(alpha=.1,
              shape=16,
-             size=0.8)+
+             size=1.2)+
   geom_smooth(method = "lm",
               formula = y~x,
               color="#993404",
@@ -69,19 +66,24 @@ Plot<-ggplot(data=ForPlot,
            size=3.5,
            family="ArielMT")+
   xlab("Number of contigs")+
-  ylab("Number of found defense systems")+
+  ylab("Number of found defence systems")+
   theme_classic()+
   theme(axis.text = element_text(size=10, family="ArielMT"))
-  
+
 Plot
 
 
-ggsave("Ecoli_numPredictedDefence_vs_numContigs.svg",
+ggsave("../figures/Ecoli_numContigs_vs_numDefence/Ecoli_numPredictedDefence_vs_numContigs.svg",
        plot=Plot,
-       width=20, height=20, units="cm", dpi=300)
-ggsave("Ecoli_numPredictedDefence_vs_numContigs.png",
+       width=20, height=18, units="cm", dpi=300)
+ggsave("../figures/Ecoli_numContigs_vs_numDefence/Ecoli_numPredictedDefence_vs_numContigs.png",
        plot=Plot,
-       width=20, height=20, units="cm", dpi=300)
+       width=20, height=18, units="cm", dpi=300)
+
+
+
+
+
 
 
 
